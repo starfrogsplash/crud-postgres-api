b3f4c3474be8842002cda62b0c2382ed8815a8e9
starfrogsplash <ali_khalil@hotmail.co.uk>
2022-01-02 00:16:48 +0000

    added postgress in docker and running sequlise orm

--- ReadMe.md
+++ ReadMe.md
@@ -2,8 +2,20 @@
 CRUD API
 ###
 
-TypeScript, jest, sequalise, postgres
+TypeScript, jest, sequalise, postgres, Docker
 
 ###
 
+instructions
 ###
+
+npm install
+
+###
+to run
+###
+```
+docker-compose up
+```
+npm start
+
--- app.ts
+++ app.ts
@@ -1,6 +1,34 @@
 import express from 'express'
 import { json } from 'body-parser'
 import {userRouter} from './routes/users'
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('docker-db', 'dbUser', 'dbPass', {
+    host: 'localhost',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    retry: {
+      // match: [Sequelize],
+      max: 3
+    }
+  });
+
+  const connectionTest = async () => {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+    } catch (err) {
+      console.error("Unable to connect to the database:", err);
+    }
+  };
+  
+  connectionTest();
+  
 
 const app = express()
 
