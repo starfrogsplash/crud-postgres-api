c7f6a44a44fd5c94e3d25069bbb35a506d6360ec
starfrogsplash <ali_khalil@hotmail.co.uk>
2022-01-02 19:35:52 +0000

    changed: switched database orm to knex and objection

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,35 @@
+require('dotenv').config()
 import express from 'express'
 import { json } from 'body-parser'
-import {userRouter} from './routes/users'
-import { Sequelize } from 'sequelize';
+import { userRouter } from './routes/users'
+import Knex from 'knex'
+import { Model } from 'objection'
 
-const sequelize = new Sequelize('docker-db', 'dbUser', 'dbPass', {
-    host: 'localhost',
-    dialect: 'postgres',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-    retry: {
-      // match: [Sequelize],
-      max: 3
-    }
-  });
+const knex = Knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    database: 'docker-db',
+    port: 5432,
+    password: process.env.DB_PASS,
+    user: process.env.DB_USER,
+  },
+})
 
-  const connectionTest = async () => {
-    try {
-      await sequelize.authenticate();
-      console.log("Connection has been established successfully.");
-    } catch (err) {
-      console.error("Unable to connect to the database:", err);
-    }
-  };
-  
-  connectionTest();
-  
+// Connect database to Objection
+Model.knex(knex)
+
+const connectionTest = async () => {
+  try {
+    const connection = await knex.raw("SELECT 1 + 1");
+    console.log(connection.rows);
+    console.log("Connection has been established successfully.");
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
+  }
+};
+
+connectionTest();
 
 const app = express()
 
